778c08dc3be1f9ffbec614d1e6237b9d5d06ed44
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2021-10-29 14:43:06 +1100

    setup precommit hooks

--- frontend/src/Dashboard/dashboard.test.js
+++ frontend/src/Dashboard/dashboard.test.js
@@ -1,24 +1,24 @@
 import {act} from "@testing-library/react";
+import {backendAPI} from "./backendAPI";
+import {BrowserRouter} from "react-router-dom";
 
 const {render, screen} = require("@testing-library/react");
 const {Dashboard} = require("./dashboard");
-import {backendAPI} from "./backendAPI";
-import {BrowserRouter} from "react-router-dom";
 
 jest.mock("./backendAPI")
 
 
-describe('Dashboard', function () {
+describe('Dashboard', () => {
     it('should have greetings', async function () {
         backendAPI.getData.mockResolvedValue(response)
-        await act(async () => {
-            await render(<BrowserRouter><Dashboard/></BrowserRouter>)
+        act(() => {
+            render(<BrowserRouter><Dashboard/></BrowserRouter>,{})
         })
         expect(screen.getByText('Header')).toBeInTheDocument();
     });
 
     const response = {
-        "Test":{}
+        "Test": {}
     }
 
 });
